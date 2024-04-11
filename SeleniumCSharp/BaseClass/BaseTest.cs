45139d51e3a4a4cdefe1820ecfa3368fb84ae29f
Arunkumar <arunkumar041093@gmail.com>
2024-04-11 12:11:18 +1200

    Initial Commit

---
+++ SeleniumCSharp/BaseClass/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharp.BaseClass
+{
+	public class BaseTest
+	{
+		public IWebDriver driver;
+        [OneTimeSetUp]
+		public void Open()
+		{
+            driver = new ChromeDriver();
+			driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com";
+        }
+
+		[OneTimeTearDown]
+		public void Close()
+		{
+            driver.Quit();
+        }
+	}
+}
+
---
+++ SeleniumCSharp/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace SeleniumCSharp;
+using OpenQA.Selenium;
+using BaseClass;
+
+[TestFixture]
+public class Tests : BaseTest
+{
+    [Test]
+    public void Test1()
+    {
+        IWebElement emailTestField = driver.FindElement(By.XPath(".//*[@id='email']"));
+        emailTestField.SendKeys("C#");
+        
+    }
+}
